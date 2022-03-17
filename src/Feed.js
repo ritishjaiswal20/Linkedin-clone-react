889716a820ae1db7611a1b4cf9650568862ff8a7
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-03-18 02:06:33 +0530

    feed input completed

--- src/Feed.js
+++ src/Feed.js
@@ -1,7 +1,39 @@
 import React from "react";
-
+import CreateIcon from "@mui/icons-material/Create";
+import "./Feed.css";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import InputOptions from "./InputOptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feed_inputContainer">
+        <div className="feed_input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed_inputOptions">
+          <InputOptions Icon={ImageIcon} title="photo" color="#70B5F9" />
+          <InputOptions
+            Icon={SubscriptionsIcon}
+            title="Video"
+            color="#E7A33E"
+          />
+          <InputOptions Icon={EventNoteIcon} title="Event" color="#7FC15E" />
+          <InputOptions
+            Icon={CalendarViewDayIcon}
+            title="Event"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
