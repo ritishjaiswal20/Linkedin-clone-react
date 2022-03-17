47c996bd3b6ebbbe9d0e98f69b068c74b852860e
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-03-17 19:48:02 +0530

    sidebar completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <div className="app_body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,12 @@ import React from "react";
 import { Avatar } from "@mui/material";
 import "./Sidebar.css";
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar_recentItems">
+      <span className="sidebar_hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
   return (
     <div className="sidebar">
       <div className="sidebar_top">
@@ -27,7 +33,13 @@ function Sidebar() {
       </div>
 
       <div className="sidebar_bottom">
-        <p>recent</p>
+        <p>Recent</p>
+        {recentItem("javascript")}
+        {recentItem("pogrammers")}
+        {recentItem("software engeenring")}
+        {recentItem("react js")}
+        {recentItem("next js")}
+        {recentItem("mern")}
       </div>
     </div>
   );
